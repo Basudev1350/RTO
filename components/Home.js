23a3d6708bb948e649cf435e574da29edf5bcd40
Basudev1350 <basudevdebnath1350@gmail.com>
2022-03-02 10:29:56 +0530

    commit

--- components/Home.js
+++ components/Home.js
@@ -24,32 +24,38 @@ class Home extends Component {
            <TouchableOpacity onPress={() => this.props.navigation.navigate('Questions')}>
            <View style = {styles.box1} >
            <View style = {styles.box12}>
-            <Image source={{uri: 'https://image.flaticon.com/icons/png/512/224/224641.png'}}
-            style={{width: 50, height: 50}} />
+             <Image
+            style={{width: 50, height: 50}} 
+            source={require('../assets/224641.png')}
+           />
            <Text style = {styles.boxfont}>Banca delle domande</Text>
            </View>
            <View style = {styles.box2}>
            <Text style = {styles.boxsubfont}>Elenco di domande e risposte e significato dei segnali stradali</Text>
            </View>
            </View>
-         </TouchableOpacity >
+         </TouchableOpacity>
          <TouchableOpacity onPress={() => this.props.navigation.navigate('Practice')}>
            <View style = {styles.box1}>
            <View style = {styles.box12}>
-            <Image source={{uri: 'https://image.flaticon.com/icons/png/512/1205/1205526.png'}}
-            style={{width: 50, height: 50}} />
+           <Image
+            style={{width: 50, height: 50}} 
+            source={require('../assets/TestIcon.png')}
+           />
            <Text style = {styles.boxfont}>Pratica</Text>
            </View>
            <View style = {styles.box2}>
            <Text style = {styles.boxsubfont}>Metti alla prova le tue conoscenze senza preoccuparti del tempo</Text>
            </View>
            </View>
-         </TouchableOpacity >
+         </TouchableOpacity>
          <TouchableOpacity onPress={() => this.props.navigation.navigate('Exam')}>
            <View style = {styles.box1} >
            <View style = {styles.box12}>
-           <Image source={{uri: 'https://freepngimg.com/thumb/paper_sheet/50192-9-exam-image-hq-image-free-png.png'}}
-            style={{width: 50, height: 50}} />
+           <Image
+            style={{width: 50, height: 50}} 
+            source={require('../assets/50192.png')}
+           />
            <Text style = {styles.boxfont}>Esame</Text>
            </View>
            <View style = {styles.box2}>
